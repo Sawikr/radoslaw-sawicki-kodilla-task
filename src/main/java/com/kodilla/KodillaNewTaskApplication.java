03fadc91e060080c8a5829ae186c657c45c7956b
Radoslaw Sawicki <sawikr@op.pl>
2022-12-01 23:22:46 +0100

    newTask

---
+++ src/main/java/com/kodilla/KodillaNewTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaNewTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaNewTaskApplication.class, args);
+    }
+
+}
