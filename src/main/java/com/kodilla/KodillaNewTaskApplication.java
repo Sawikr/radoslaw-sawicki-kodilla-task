35e0955afb488fa0df241f280d9b7a3f59a0e37d
Radoslaw Sawicki <sawikr@op.pl>
2022-12-01 23:31:02 +0100

    newChange

--- src/main/java/com/kodilla/KodillaNewTaskApplication.java
+++ src/main/java/com/kodilla/KodillaNewTaskApplication.java
@@ -8,6 +8,7 @@ public class KodillaNewTaskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaNewTaskApplication.class, args);
-    }
 
+        System.out.println("New data");
+    }
 }
